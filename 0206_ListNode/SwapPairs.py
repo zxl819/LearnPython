2339f3bdfd937f91c439bb758fcf53e1450fb13d
zxl819 <79512237+zxl819@users.noreply.github.com>
2024-02-06 21:05:24 +0800

    Update SwapPairs.py

--- 0206_ListNode/SwapPairs.py
+++ 0206_ListNode/SwapPairs.py
@@ -4,4 +4,23 @@ class ListNode:
         self.val = val
 
 class Solution:
+<<<<<<< Updated upstream
     def swapPairs(self, head: ListNode):
+=======
+    def swapPairs(self, head: ListNode):
+        dummpy_head = ListNode(next=head)
+        current = dummpy_head
+        while current.next and current.next.next:
+            
+            tmp1 = current.next.next
+            tmp = current.next
+
+            current.next = current.next.next
+            current.next.next = 
+    
+            tmp.next = tmp1
+            current.next = current.next.next
+
+        return dummy_head.next
+    
+>>>>>>> Stashed changes
