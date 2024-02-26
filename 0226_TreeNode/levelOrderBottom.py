ef65445f2eb1f122e463e485b82b123109eba274
zxl819 <79512237+zxl819@users.noreply.github.com>
2024-02-26 21:49:33 +0800

    new0226

---
+++ 0226_TreeNode/levelOrderBottom.py
@@ -0,0 +1,24 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def levelOrderBottom(self, root: TreeNode):
+        from collections import deque
+        queue = deque()
+        res = []
+        queue.append(root)
+        while queue:
+            level = []
+            for _ in range(queue):
+                node = queue.popleft()
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            res.append(level)
+        return res
+
+
