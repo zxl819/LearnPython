307943020d6e94df9e377c95d5a5a8f071035b4c
zxl819 <79512237+zxl819@users.noreply.github.com>
2024-03-06 21:15:51 +0800

    HASH NEW

---
+++ 0213_hashTable1/longestConsecutive.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        res = 0
+        num_set = set(nums)
+        for num in num_set:
+            if (num - 1) not in num_set:
+                seq_len = 1
+                while (num + 1) in num_set:
+                    seq_len += 1
+                    num += 1
+                res = max(res, seq_len)
+        return res
+
+            
